772a1e155dbf1022d8c3acdb7290e59a811673a5
M Madhumithaa <68317503+madhu915@users.noreply.github.com>
2022-07-14 13:57:40 +0000

    Sync LeetCode submission - Maximum Product of Three Numbers (cpp)

---
+++ my-folder/problems/maximum_product_of_three_numbers/solution.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int a=nums[n-1]*nums[n-2]*nums[n-3];
+        int b=nums[0]*nums[1]*nums[n-1];
+        return a>b?a:b;
+    }
+};
