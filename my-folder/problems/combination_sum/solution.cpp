21a8c0b1e6592e3efe54941d7bd1819ac3603ab2
M Madhumithaa <68317503+madhu915@users.noreply.github.com>
2022-07-17 08:34:55 +0000

    Sync LeetCode submission - Combination Sum (cpp)

---
+++ my-folder/problems/combination_sum/solution.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int> > solution;
+    void combsum(vector<int>& candidates,int index,vector<int> curr,int target){
+        if(target<0)
+            return;
+        if(target==0)
+            solution.push_back(curr);
+        for(int i=index; i<candidates.size(); i++){
+            curr.push_back(candidates[i]);
+            combsum(candidates,i,curr,target-candidates[i]);
+            curr.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> curr;
+        if(candidates.size()==0 || target<0)
+            return solution;
+        sort(candidates.begin(),candidates.end());
+        combsum(candidates,0,curr,target);
+        return solution;
+    }
+};
