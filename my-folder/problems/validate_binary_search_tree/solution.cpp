5fab8b176349a6e7c1248ca8f3012ef8a8da2ada
M Madhumithaa <68317503+madhu915@users.noreply.github.com>
2022-08-26 19:35:33 +0000

    Sync LeetCode submission - Validate Binary Search Tree (cpp)

--- my-folder/problems/validate_binary_search_tree/solution.cpp
+++ my-folder/problems/validate_binary_search_tree/solution.cpp
@@ -11,20 +11,29 @@
  */
 class Solution {
 public:
-    vector<int>trav;
-    void inorder(TreeNode* root){
-        if(root==NULL)
-            return;
-        inorder(root->left);
-        trav.push_back(root->val);
-        inorder(root->right);
-    }
+    // vector<int>trav;
+    // void inorder(TreeNode* root){
+        // if(root==NULL)
+        //     return;
+        // inorder(root->left);
+        // trav.push_back(root->val);
+        // inorder(root->right);
+    // }
     bool isValidBST(TreeNode* root) {
-        inorder(root);
-        for(int i=0;i<trav.size()-1;i++){            
-            if(trav[i]>=trav[i+1])
-                return false;
-        }
-        return true;
+    //     inorder(root);
+    //     for(int i=0;i<trav.size()-1;i++){            
+    //         if(trav[i]>=trav[i+1])
+    //             return false;
+    //     }
+    //     return true;
+        
+        return isValidBST(root,LONG_MIN,LONG_MAX);        
+    }
+    bool isValidBST(TreeNode* root,long long min,long long max){
+        if(root==NULL)
+            return true;
+        if(root->val>=max || root->val<=min)
+            return false;
+        return isValidBST(root->left,min,root->val) && isValidBST(root->right,root->val,max);
     }
 };
