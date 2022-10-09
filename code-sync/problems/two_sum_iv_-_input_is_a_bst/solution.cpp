ffd113fcd43b806f2e37c7dcecd38d2dc82cea59
M Madhumithaa <68317503+madhu915@users.noreply.github.com>
2022-10-09 07:14:23 +0000

    Sync LeetCode submission - Two Sum IV - Input is a BST (cpp)

--- code-sync/problems/two_sum_iv_-_input_is_a_bst/solution.cpp
+++ code-sync/problems/two_sum_iv_-_input_is_a_bst/solution.cpp
@@ -9,30 +9,55 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class BSTIterator {
+    stack<TreeNode *> myStack;
+    bool reverse = true; 
+public:
+    BSTIterator(TreeNode *root, bool isReverse) {
+        reverse = isReverse; 
+        pushAll(root);
+    }
+
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return !myStack.empty();
+    }
+
+    /** @return the next smallest number */
+    int next() {
+        TreeNode *tmpNode = myStack.top();
+        myStack.pop();
+        if(!reverse) pushAll(tmpNode->right);
+        else pushAll(tmpNode->left);
+        return tmpNode->val;
+    }
+
+private:
+    void pushAll(TreeNode *node) {
+        for(;node != NULL; ) {
+             myStack.push(node);
+             if(reverse == true) {
+                 node = node->right; 
+             } else {
+                 node = node->left; 
+             }
+        }
+    }
+};
 class Solution {
 public:
-    vector<int>inor;
-    int s;
     bool findTarget(TreeNode* root, int k) {
+        if(!root) return false; 
+        BSTIterator l(root, false); 
+        BSTIterator r(root, true); 
         
-        inorder(root);      
-        int st=0,end=inor.size()-1;
-        cout<<end;
-        while(st<end){
-            if(k==inor[st]+inor[end])
-                return true;
-            if(k<inor[st]+inor[end])
-                end--;
-            else
-                st++;
+        int i = l.next(); 
+        int j = r.next(); 
+        while(i<j) {
+            if(i + j == k) return true; 
+            else if(i + j < k) i = l.next(); 
+            else j = r.next(); 
         }
-        return false;
-    }
-    void inorder(TreeNode* root){
-        if(!root)
-            return;
-        inorder(root->left);
-        inor.push_back(root->val);
-        inorder(root->right);
+        return false; 
     }
 };
